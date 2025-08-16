8c620adbd1617cdafb8e4f8ab27c042d92ab99bb
Bala9840 <barathbalaji9840@gmail.com>
2025-08-16 21:42:59 +0530

    Remove BOM for scroll

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,4 @@
-﻿import React from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import styles from './Sidebar.module.css';
